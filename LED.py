ada8100e174f4566699f31d0f55037237cfb4b60
LoganPC <loganwade817@gmail.com>
2026-01-14 19:10:42 -0700

    GOT IT SUCKERS

--- LED.py
+++ LED.py
@@ -6,26 +6,19 @@ from time import sleep
 # The second argument is the number of LEDs (1 for a single built-in)
 np = NeoPixel(Pin(48), 1) # Use Pin(38) if 48 doesn't work [11]
 
-volt = Pin(14, Pin.OUT)
-
-
 while True:
-    np[0] = (10, 0, 0) # Set Red (R, G, B)
+    np[0] = (1, 0, 0) # Set Red (R, G, B)
     np.write()          # Send data to the LED
-    sleep(.5)
+    sleep(1)
 
-    np[0] = (0, 10, 0) # Set Green
+    np[0] = (0, 1, 0) # Set Green
     np.write()
-    sleep(.5)
+    sleep(1)
 
-    np[0] = (0, 0, 10) # Set Blue
+    np[0] = (0, 0, 1) # Set Blue
     np.write()
-    sleep(.5)
+    sleep(1)
 
     np[0] = (0, 0, 0) # Turn off (Black)
     np.write()
-    
-    volt.on()
-    sleep(5)
-    volt.off()
-    break
+    sleep(1)
